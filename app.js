ab0354f0dcb99b76abda77a34ea211a4b87a59cb
bryantee <bryantee@gmail.com>
2016-09-21 14:41:02 -0700

    Get word tokenization working
    Functions to strip out extra characters and create array for words. To be used by next all functions going forward.

--- app.js
+++ app.js
@@ -17,3 +17,13 @@ function calculateAverageWordLength(text) {
 function calculateAverageSentenceLength(text) {
 
 }
+
+function tokenizeText(text) {
+    // split words into array and sort
+    return text.toLowerCase().split(/[\s]+/).filter(Boolean).sort();
+}
+
+function stripExtras(text) {
+    // strips out everything except whitespace, numbers or letters
+    return text.replace(/[^\w\s]|_/g, "");
+}
